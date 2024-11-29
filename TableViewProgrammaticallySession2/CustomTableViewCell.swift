779c5382fe85194f04dbebae25584eb810732100
Matthew Dolan <mdolancode@gmail.com>
2024-11-28 20:11:24 -0800

    Setup tableView and custom cell.

---
+++ TableViewProgrammaticallySession2/CustomTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomTableViewCell.swift
+//  TableViewProgrammaticallySession2
+//
+//  Created by Matthew Dolan on 2024-11-28.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    var label = UILabel()
+}
--- TableViewProgrammaticallySession2/ViewController.swift
+++ TableViewProgrammaticallySession2/ViewController.swift
@@ -9,9 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView = UITableView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemIndigo
+        setupTableView()
+        tableViewConstraints()
+        registerCell()
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableViewConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    func registerCell() {
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        return cell
     }
 }
 
